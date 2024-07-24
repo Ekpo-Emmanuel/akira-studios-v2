1d340e058e70793fe091bad226a97565a3ca4826
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-07-24 15:27:21 -0700

    feat: about page

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Hero from '../_components/About Page/Hero'
+import { MarqueeDemoVertical } from '../_components/About Page/HeroAnimated'
+
+export default function page() {
+  return (
+    <div>
+      <Hero />
+    </div>
+  )
+}
