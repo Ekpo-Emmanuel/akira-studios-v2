ddc03083898a2003958b62e7ef49d7df1ed8b57d
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-07-25 12:40:33 -0700

    feat: Added extra components

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import Hero from '../_components/About Page/Hero'
 import { MarqueeDemoVertical } from '../_components/About Page/HeroAnimated'
+import About from '../_components/About Page/About'
+import Services from '../_components/About Page/Services'
+import TextRevealByWord from '../_components/magic ui/TextRevealByWord'
 
 export default function page() {
   return (
     <div>
       <Hero />
+      <About />
+      <Services />
+      <TextRevealByWord 
+        text="Our team is more than just a group of professionals; we're a family of creative minds dedicated to making your dreams a reality. We approach each project with the same enthusiasm and dedication as if it were our own." 
+      />
     </div>
   )
 }
