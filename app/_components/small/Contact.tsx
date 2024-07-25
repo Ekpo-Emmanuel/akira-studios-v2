c3ca309c26ed34aac4e11b8e809518abb52c84c7
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-07-25 16:52:00 -0700

    feat: add interactive hover effect

---
+++ app/_components/small/Contact.tsx
@@ -0,0 +1,26 @@
+'use client';
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+
+const Contact = () => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <div
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <p className="text-[1.125rem] cursor-pointer">contact@akira.com</p>
+      <div className="mt-1 h-[2px] w-full bg-darkyellow rounded-full overflow-hidden">
+        <motion.div
+          className="h-[2px] bg-red rounded-full"
+          initial={{ width: "20%" }}
+          animate={{ width: isHovered ? "100%" : "20%" }}
+          transition={{ duration: .5, ease: "easeInOut" }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
