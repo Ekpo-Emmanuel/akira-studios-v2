9265adc9d2c2f5d065eb05fd579d0db54d519784
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-07-25 15:11:41 -0700

    feat: Added FAQ component

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,6 +4,7 @@ import { MarqueeDemoVertical } from '../_components/About Page/HeroAnimated'
 import About from '../_components/About Page/About'
 import Services from '../_components/About Page/Services'
 import TextRevealByWord from '../_components/magic ui/TextRevealByWord'
+import Faq from '../_components/Faq'
 
 export default function page() {
   return (
@@ -14,6 +15,7 @@ export default function page() {
       <TextRevealByWord 
         text="Our team is more than just a group of professionals; we're a family of creative minds dedicated to making your dreams a reality. We approach each project with the same enthusiasm and dedication as if it were our own." 
       />
+      <Faq />
     </div>
   )
 }
