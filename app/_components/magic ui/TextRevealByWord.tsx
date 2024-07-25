bc5690534179663064605e7aba1619d68fed9e57
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-07-25 12:40:17 -0700

    feat: Implement TextRevealByWord component with scroll-based text reveal

---
+++ app/_components/magic ui/TextRevealByWord.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import { FC, ReactNode, useRef } from "react";
+import localFont from "next/font/local";
+import { motion, useScroll, useTransform } from "framer-motion";
+
+import { cn } from "@/lib/utils";
+
+const bentoga = localFont({
+  src: "../../../public/fonts/bentoga/Bentoga-Thin.otf",
+});
+const satoshi = localFont({
+  src: [
+    {
+      path: "../../../public/fonts/Satoshi/Fonts/OTF/Satoshi-Medium.otf",
+      weight: "500",
+      style: "medium",
+    },
+    {
+      path: "../../../public/fonts/Satoshi/Fonts/OTF/Satoshi-Light.otf",
+      weight: "300",
+      style: "light",
+    },
+  ],
+});
+
+interface TextRevealByWordProps {
+  text: string;
+  className?: string;
+}
+
+export const TextRevealByWord: FC<TextRevealByWordProps> = ({
+  text,
+  className,
+}) => {
+  const targetRef = useRef<HTMLDivElement | null>(null);
+
+  const { scrollYProgress } = useScroll({
+    target: targetRef,
+  });
+  const words = text.split(" ");
+
+  return (
+    <div ref={targetRef} className={cn("relative z-0 h-[200vh]", className)}>
+      <div
+        className={
+          "sticky top-0 mx-auto flex h-[50%] max-w-7xl items-center px-4 py-[5rem]"
+        }
+      >
+        <p
+          ref={targetRef}
+          className={
+            `${bentoga.className} text-center flex flex-wrap text-[1.8rem] sm:text-[5vw] lg:text-[3.5rem] leading-[1.3] md:leading-[1.2] text-red/30`
+          }
+        >
+          {words.map((word, i) => {
+            const start = i / words.length;
+            const end = start + 1 / words.length;
+            return (
+              <Word key={i} progress={scrollYProgress} range={[start, end]}>
+                {word}
+              </Word>
+            );
+          })}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+interface WordProps {
+  children: ReactNode;
+  progress: any;
+  range: [number, number];
+}
+
+const Word: FC<WordProps> = ({ children, progress, range }) => {
+  const opacity = useTransform(progress, range, [0, 1]);
+  return (
+    <span className="xl:lg-3 relative mx-1 lg:mx-2.5">
+      <span className={"absolute opacity-30"}>{children}</span>
+      <motion.span
+        style={{ opacity: opacity }}
+        className={"text-red"}
+      >
+        {children}
+      </motion.span>
+    </span>
+  );
+};
+
+export default TextRevealByWord;
