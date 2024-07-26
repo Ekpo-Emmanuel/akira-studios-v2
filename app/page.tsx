82f2b0dcd45dd7337cdcea307b534bd99d81b215
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-07-25 17:25:38 -0700

    feat: added new navigation

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,13 @@ import Hero from "./_components/Hero";
 import About from "./_components/About";
 import Projects from "./_components/Projects";
 import About2 from "./_components/About2";
+import Navbar2 from "./_components/Navbar/Navbar2";
 
 export default function Home() {
   return (
    <div>
-    <Navbar />
+    {/* <Navbar /> */}
+    <Navbar2 />
     <Hero />
     <About />
     <About2 />
