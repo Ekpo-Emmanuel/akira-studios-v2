786065d6379beffd513f19ffb73dab960adf6c31
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-07-26 11:14:50 -0700

    feat: added thankyou component for contact page

---
+++ app/_components/Contact page/Thankyou.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ThankYou() {
+  return (
+    <div className='text-lightyellow flex items-center justify-center h-fit p-[20px] bg-red text-[1.2rem] text-center font-bold uppercase rounded-[1rem]  max-w-2xl mx-auto'>
+      <h1>THANKS FOR REACHING OUT. WE WILL GET BACK TO YOU SOON.</h1>
+    </div>
+  )
+}
