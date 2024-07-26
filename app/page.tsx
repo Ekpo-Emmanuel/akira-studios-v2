e6743d13a4c5f13d24213b40b3436feb8ef68a84
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-07-26 11:24:27 -0700

    refactor: removed Navbar from page

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,11 @@
-import Image from "next/image";
-import Navbar from "./_components/Navbar/Navbar";
 import Hero from "./_components/Hero";
 import About from "./_components/About";
 import Projects from "./_components/Projects";
 import About2 from "./_components/About2";
-import Navbar2 from "./_components/Navbar/Navbar2";
 
 export default function Home() {
   return (
    <div>
-    {/* <Navbar /> */}
-    <Navbar2 />
     <Hero />
     <About />
     <About2 />
