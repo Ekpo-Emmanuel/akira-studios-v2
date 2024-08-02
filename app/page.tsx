921459eab09b01743a278146105dc4c5c957f55e
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-08-02 15:03:50 -0700

    feat(Home): add Navbar and Footer components to main layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,18 @@ import Hero from "./_components/Hero";
 import About from "./_components/About";
 import Projects from "./_components/Projects";
 import About2 from "./_components/About2";
+import Navbar2 from './_components/Navbar/Navbar2'
+import Footer from './_components/Footer/Footer'
 
 export default function Home() {
   return (
    <div>
+    <Navbar2 />
     <Hero />
     <About />
     <About2 />
     <Projects />
+    <Footer />
    </div>
   );
 }
