27a6c85f6dd7c0b1299e97f33e628056e48c8a91
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-08-02 15:12:53 -0700

    style: changed Font

--- app/works/page.tsx
+++ app/works/page.tsx
@@ -4,6 +4,16 @@ import localFont from "next/font/local";
 import React, { useState } from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import { Anton } from 'next/font/google'
+import Navbar2 from '../_components/Navbar/Navbar2'
+import Footer from '../_components/Footer/Footer'
+
+
+const anton = Anton({
+  subsets: ['latin'],
+  weight: '400',
+  variable: '--font-anton',
+})
 
 const bentoga = localFont({ src: "../../public/fonts/bentoga/Bentoga-Thin.otf" });
 const satoshi = localFont({
@@ -50,44 +60,47 @@ export default function Page() {
   };
 
   return (
-    <motion.div 
-      className="w-full h-screen flex items-center justify-center"
-      initial="hidden"
-      animate="visible"
-      variants={containerVariants}
-    >
-      <div className="grid gap-[1rem] px-4">
-        <motion.h2
-          className={clsx(
-            bentoga.className,
-            "antialiased text-[5.875rem] md:text-[8.875rem] uppercase leading-[1.1] text-center"
-          )}
-          variants={itemVariants}
-          onHoverStart={() => setIsHovered(true)}
-          onHoverEnd={() => setIsHovered(false)}
-        >
-          {Array.from("Coming soon").map((letter, index) => (
-            <motion.span
-              key={index}
-              variants={letterVariants}
-              animate={isHovered ? "hover" : ""}
-              custom={index}
-              style={{ display: 'inline-block' }}
-            >
-              {letter}
-            </motion.span>
-          ))}
-        </motion.h2>
-        <motion.p
-          className={clsx(
-            bentoga.className,
-            "antialiased text-[32px] leading-[36px] text-center uppercase"
-          )}
-          variants={itemVariants}
-        >
-          page under construction
-        </motion.p>
-      </div>
-    </motion.div>
+    <>
+    <Navbar2 />
+      <motion.div 
+        className="w-full h-screen flex items-center justify-center"
+        initial="hidden"
+        animate="visible"
+        variants={containerVariants}
+      >
+        <div className="grid gap-[1rem] px-4 cursor-default">
+          <motion.h2
+            className={clsx(
+              anton.className,
+              "antialiased text-[5.875rem] md:text-[8.875rem] uppercase leading-[1.1] text-center"
+            )}
+            variants={itemVariants}
+            onHoverStart={() => setIsHovered(true)}
+            onHoverEnd={() => setIsHovered(false)}
+          >
+            {Array.from("Works/").map((letter, index) => (
+              <motion.span
+                key={index}
+                variants={letterVariants}
+                animate={isHovered ? "hover" : ""}
+                custom={index}
+                style={{ display: 'inline-block' }}
+              >
+                {letter}
+              </motion.span>
+            ))}
+          </motion.h2>
+          <motion.p
+            className={clsx(
+              "antialiased text-[22px] leading-[36px] text-center uppercase"
+            )}
+            variants={itemVariants}
+          >
+            page under construction
+          </motion.p>
+        </div>
+      </motion.div>
+    <Footer />
+    </>
   );
 }
